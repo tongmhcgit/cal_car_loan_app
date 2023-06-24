66c2b6954cf6ac1b8ca47ab4fc55bb994e5f9eec
tongmhcgit <tongmhcgit@gmail.com>
2023-06-24 19:36:45 +0700

    1st commit

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatefulWidget {
+  const HomeUi({super.key});
+
+  @override
+  State<HomeUi> createState() => _HomeUiState();
+}
+
+class _HomeUiState extends State<HomeUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
