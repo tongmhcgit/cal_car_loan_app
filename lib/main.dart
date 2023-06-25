e632a2960ad1e1e6d0ce710e9494c19d875e3659
tongmhcgit <tongmhcgit@gmail.com>
2023-06-25 11:05:43 +0700

    3 commit add carpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cal_car_loan_app/views/introduction_ui.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,6 +8,7 @@ void main() {
     //เรียก widget หลักของแอปฯ แนะนำ MaterialApp
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: IntroductionUI (),
     ),
   );
 }
